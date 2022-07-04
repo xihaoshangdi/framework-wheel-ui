d54024dd446d489a8f2f10e3e779020b652852d1
xihaoshangdi <xihaoshangdi@163.com>
2022-07-04 11:48:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <my-button>我是自定义按钮</my-button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import MyKit from '../packages';
+
+createApp(App).use(MyKit).mount('#app')
